60b0b4e4960fe4199e51a652fad3dc81376cde47
Maxim Alpatski <MaximAlpatski@gmail.com>
2023-06-06 11:19:58 +0400

    Finished account script and created topbar generation

--- scripts/account.js
+++ scripts/account.js
@@ -1,12 +1,59 @@
 function loadData() {
-    let xhttp = XMLHttpRequest();
-    xhttp.onReadyStateChange = function() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             loadAccountInfo(xhttp);
+            loadOrders(xhttp);
         }
     }
+    xhttp.open("GET", "/data/account.json", true);
+    xhttp.send();
 }
 
-function loadAccountInfo() {
+function loadAccountInfo(xhttp) {
+    let rootJson = xhttp.responseText;
+    let rootObj = JSON.parse(rootJson);
+    let accountData = rootObj.account;
 
+    document.getElementById("email").innerHTML = "Email: " + accountData.email;
+    document.getElementById("name").innerHTML = "Name: " + accountData.name;
+    document.getElementById("phoneNumber").innerHTML = "Phone number: " + accountData.phoneNumber;
+    document.getElementById("address").innerHTML = "Address: " + accountData.address;
+    document.getElementById("dateOfBirth").innerHTML = "Date of birth: " + accountData.dateOfBirth;
+    document.getElementById("avatar").src = accountData.avatarSrc;
+}
+
+function loadOrders(xhttp) {
+    let rootJson = xhttp.responseText;
+    let rootObj = JSON.parse(rootJson);
+
+    let list = document.getElementById("orders_list");
+    list.innerHTML = generateOrders(rootObj.account.orders)
+}
+
+function generateOrders(orderDataList) {
+    let result = "";
+    for (i = 0; i < orderDataList.length; i++) {
+        result += generateOrder(orderDataList[i]);
+    }
+    return result;
+}
+
+function generateOrder(orderData) {
+    return `
+        <div class="item_card"> 
+            <img class="item_image" src="${orderData.imgSrc}" />
+            <div class="item_label">
+                <p class="item_name">
+                    ${orderData.name}
+                </p>
+                <p class="item_quantity">
+                    Quantity: ${orderData.quantity}
+                </p>
+                <p class="order_date">
+                    Date: ${orderData.date}
+                </p>
+            </div>
+        </div>
+    `
 }
