60dab4ed05d11cd93667742ad43d9c700a3219a8
Maxim Alpatski <MaximAlpatski@gmail.com>
2023-06-21 23:51:20 +0400

    Added basic post request

--- node/server.js
+++ node/server.js
@@ -1,7 +1,5 @@
-const express = require('express')
-const url = require("url");
-const fs = require("fs");
-const querystring = require("querystring");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const loginModule = require("./login");
 const signUpModule = require("./signUp");
@@ -15,14 +13,15 @@ const paymentModule = require("./payment");
 const homeModule = require("./home");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/login", function(req, res) {
     console.log("login");
     loginModule.showLogin(res);
 });
 app.get("/sign_up", function(req, res) {
-    console.log("sing_up");
-    signUpModule.showSignUp(res);
+    console.log("sign_up");
+    signUpModule.showSignUp(req, res);
 });
 app.get("/account", function(req, res) {
     console.log("account");
@@ -58,8 +57,11 @@ app.get("/", function(req, res) {
 });
 
 app.post("/homeClick", function(req, res) {
-    console.log("home");
-    homeModule.proceedHomeClick(res);
+    homeModule.proceedHomeClick(res); 
+});
+app.post("/signUpClick", function(req, res) {
+    console.log(req.body);
+    signUpModule.proceedSignUpClick(req, res);
 });
 
 app.use(express.static("./"));
---
+++ node/signUp.js
@@ -0,0 +1,17 @@
+let fs = require("fs");
+
+exports.showSignUp = function(req, res) {
+    fs.readFile("./SignUp_Page.html", function (err, data) {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+    });
+};
+
+exports.proceedSignUpClick = function(req, res) {
+    console.log(req.body);
+}
