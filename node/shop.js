78ddcc78ee2cc42df4dfdd8427b60c1b4d13535f
Saad767855 <sm5566014@gmail.com>
2023-06-25 16:49:53 +0400

    done with code, but cant run

---
+++ node/shop.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql');
+const fs = require('fs');
+
+exports.showHome = function (res) {
+    fs.readFile("./shop.html", function (err, data) {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+    });
+};
+
+exports.proceedShopClick = function (res) {
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "rootroot",
+        database: "printing_store"
+    });
+
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+
+        var sql = "SELECT * FROM printing_store";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+
+            const jsonData = JSON.stringify(result);
+            fs.writeFile('blank.json', jsonData, 'utf8', function (err) {
+                if (err) throw err;
+                console.log("Written successfully");
+
+                con.end(function (err) {
+                    if (err) throw err;
+                    console.log("Database connection closed.");
+                });
+            });
+        });
+    });
+
+    console.log("User accessed shop page");
+};
