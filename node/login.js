1995c6ad6d5f65e8a3dc50407e1dfc06fd48d6d6
Maxim Alpatski <MaximAlpatski@gmail.com>
2023-06-26 20:14:38 +0400

    Login page

--- node/login.js
+++ node/login.js
@@ -15,36 +15,87 @@ exports.showLogin = function (res) {
 exports.proceedloginClick = function(req, res) {
     let userData = req.body;
 
+    console.log(userData);
+
     const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "MaxPain2001",
-    database: "printing_store"
-})
+        host: "localhost",
+        user: "root",
+        password: "MaxPain2001",
+        database: "printing_store"
+    })
 
     con.connect(function(err) {
         if (err) throw err;
         console.log("Connected")
         
-        con.query("SELECT * FROM store_user WHERE email=" + userData.email + "AND user_password=" + userData.userPassword, function(err, result) {
+        con.query(`SELECT * FROM store_user WHERE email = '${userData.email}' AND user_password = '${userData.userPassword}';`, function(err, result) {
             if (err) throw err;
-            if (result.length > 0) {
-               res.redirect("/HomePage");
-               console.log("User exists")
-               const jsonData = JSON.stringify(result[0]);
-               fs.writeFile('account.json', jsonData, (err) => {
-                if (err) throw err;
-                else {
-                    console.log("User data written to JSON");
-                }
-               })
 
+            if (result.length == 1) {
+                res.redirect("/");
+                console.log("User exists")
+
+                let userId = result[0].id;
+                let user = {
+                    email: result[0].email,
+                    name: result[0].user_name, 
+                    phoneNumber: result[0].mobile,
+                    address: result[0].address,
+                    dateOfBirth: result[0]. date_of_birth,
+                    avatarSrc: result[0].avatar_src
+                }
+
+                let sqlOrders = `SELECT * FROM user_item WHERE user_id=${userId};`;
+                con.query(sqlOrders, function(err, result) {
+                    if (err) throw err;
+
+                    let orders = new Array(result.length);
+                    for (let i = 0; i < result.length; i++) {
+                        let itemId = result[i].item_id;
+                        let itemQuantity = result[i].count;
+                        let deliveryDate = result[i].date_of_delivery;
+                        let sqlItem = `SELECT * FROM item WHERE id = ${itemId};`;
+                        con.query(sqlItem, function(err, result) {
+                            if (err) throw err;
+
+                            let item = {
+                                id: result[0].id,
+                                name: result[0].item_name,
+                                price: result[0].price,
+                                imgSrc: result[0].img_src,
+                                quantity: itemQuantity,
+                                date: deliveryDate
+                            }
+                            orders[i] = item;
+
+                            if (i == orders.length - 1) {
+                                user.orders = orders;
+                                const userJson = JSON.stringify(user);
+                                fs.writeFile('account.json', jsonData, (err) => {
+                                    if (err) throw err;
+                                    console.log("User data written to JSON");
+                                });
+                                fs.readFile("./data/account.json", function(err, data) {
+                                    let parsedData = JSON.parse(data);
+                                    parsedData.account = userJson;
+                                    fs.writeFile("./data/account.json", JSON.stringify(parsedData), function(err) {
+                                        if (err) throw err;
+
+                                        con.end();
+                                        return res.end();
+                                    });
+                                });
+                            }
+                        });
+                    }
+                });
             } else {
-               res.redirect("/login");
-               console.log("User does not exist")
+                res.redirect("/login");
+                console.log("User does not exist")
+
+                con.end();
+                return res.end();
             }
-           con.end();
-           return res.end();
        });
    });
 }
